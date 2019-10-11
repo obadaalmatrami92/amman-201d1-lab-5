f3679cb71322ef758caaa056c66ee7975c63e14f
obada almatrami <obada.matrami92@gmail.com>
2019-10-12 02:31:46 +0300

    solve-problem5

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var c = a+b ;
     var resultString = 'The sum of ' + a + ' and ' + b + ' is ' + c + '.';
     return [c, resultString];
 }
+<<<<<<< .merge_file_ikDVqd
 =======
 // function sum(a, b) { //eslint-disable-line
 //     //give 4, 7 should return {11, 'the sum of 4 and 7 is 11.}
@@ -20,6 +21,8 @@ var c = a+b ;
 // }
 
 >>>>>>> .merge_file_AtGIYg
+=======
+>>>>>>> .merge_file_OoxZWc
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -29,6 +32,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 // Write your code here
+<<<<<<< .merge_file_ikDVqd
 <<<<<<< .merge_file_fIuELh
 function multiply(a, b) { 
     }
@@ -39,6 +43,10 @@ function multiply(g, h) { //eslint-disable-line
 }
 
 >>>>>>> .merge_file_AtGIYg
+=======
+function multiply(a, b) { 
+    }
+>>>>>>> .merge_file_OoxZWc
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -94,10 +102,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 <<<<<<< .merge_file_fIuELh
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+<<<<<<< .merge_file_ikDVqd
 =======
 var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 >>>>>>> .merge_file_AtGIYg
+=======
+>>>>>>> .merge_file_OoxZWc
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 }
 // Here is the test for multiplyArray(); uncomment it to run it
