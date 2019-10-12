ea2d3495267001a2eba0c59cb6442a9215fe83db
obada almatrami <obada.matrami92@gmail.com>
2019-10-12 03:07:54 +0300

    answer2

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) {
+    var c = a * b;
+    return [c, "The product of " + a + " and " + b + " is " + c + "."];
 }
 
 // Here is the test for multiply(); uncomment it to run it
