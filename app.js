763ffa6429f2d1c9f739127dc07a15b5f9b8c1d7
obada almatrami <obada.matrami92@gmail.com>
2019-10-12 03:48:55 +0300

    answer4

--- app.js
+++ app.js
@@ -51,8 +51,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var summing = sum(a, b);
+    var summing2 = sum(summing[0], c)
+    var multiplying = multiply(a, b);
+    var multiplying2 = multiply(multiplying[0], c)
+    return [summing2[0], multiplying2[0], a + " and " + b + " and " + c + " sum to " + summing2[0] + ".", "The product of " + a + " and " + b + " and " + c + " is " + multiplying2[0] + "."];
 }
+
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
@@ -70,10 +75,12 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
-
-function sumArray(sumArr) { //eslint-disable-line
-
+var testArray = [2, 3, 4];
+function sumArray(hhArray) {
+    var hhArray = [2, 3, 4];
+    var hhSum = sum(hhArray[0], hhArray[1]);
+    var bbSum = sum(hhSum[0], hhArray[2]);
+    return [bbSum[0], "2,3,4 was passed in as an array of numbers, and 9 is their sum."]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -119,7 +126,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
